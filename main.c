4d73d4c5d8d78af528be995b04f64f629dd5d76c
camrockz <55742787+camrockz@users.noreply.github.com>
2019-09-25 17:44:38 +1000

    Update main.c
    Update licence header

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+/* Copyright 2019 camrockz https://github.com/camrockz/ */
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
